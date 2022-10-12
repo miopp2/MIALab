bc32f44eca71c333b7cb798bf98aa193daa0e1b3
miopp2 <mirjam.oppliger@gmail.com>
2022-10-12 16:47:54 +0200

    Setup for Ubelix

--- scripts/create_ubelix_env.sh
+++ scripts/create_ubelix_env.sh
@@ -11,4 +11,9 @@ conda create --name mialab python=3.8
 conda activate mialab
 
 # Install the MIALab dependencies
-pip install -r ../requirements.txt
+pip install -r requirements.txt
+
+
+# CONFLICTS:
+# daal4py 2021.3.0 requires daal==2021.2.3, which is not installed.
+# numba 0.54.1 requires numpy<1.21,>=1.17, but you have numpy 1.23.3 which is incompatible.
